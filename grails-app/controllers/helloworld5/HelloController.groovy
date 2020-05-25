5dd3fc745916fdfd0004bf8ddab6bfa388bfc9d7
cyn <cyn@cyndeMacBook-Pro.local>
2020-05-25 18:13:22 +0800

    add GET and POST request

--- grails-app/controllers/helloworld5/HelloController.groovy
+++ grails-app/controllers/helloworld5/HelloController.groovy
@@ -1,7 +1,40 @@
 package helloworld5
 
+import grails.converters.JSON
+
 class HelloController {
     def index() {
-        render "hello world!"
+        render "hello world5!"
     }
+
+    def getTest() {
+        log.info("----get: $id -----")
+        render $id
+    }
+
+    def getTest2() {
+        def id = params.id
+        log.info("----get2: $id ---")
+        render "getTest2: $id"
+    }
+
+    def postTest() {
+        def data = request.JSON
+        log.info("---post: $data ---")
+        render "post: $data"
+    }
+
+    def postTest2() {
+        def id = params.id
+        def data = request.JSON
+        log.info("---post: id: $id, data: $data ---")
+        respond "post: $data"
+    }
+
+
+//    def postTest(){
+////        def id = params.id
+//        def data = request.JSON
+//        log.info("---post param: ")
+//    }
 }
--- grails-app/controllers/helloworld5/UrlMappings.groovy
+++ grails-app/controllers/helloworld5/UrlMappings.groovy
@@ -12,5 +12,16 @@ class UrlMappings {
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
+
+//        "/template/update/$id"(controller: "mmsTemplate", action: "update", method: "POST")
+        "/get/$id"(controller:"hello", action: "getTest")
+        "/get/$id"(controller:"hello", action: "getTest2", method: "GET")//上面那个就失效了
+        "/post"(controller:"hello", action: "postTest", method: "POST")
+        // /post?id=1
+        "/post/$id"(controller:"hello", action: "postTest2", method: "POST")
+
+
+
+
     }
 }
